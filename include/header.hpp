585b9fac02a8ea6f73c03cef54bb9b7fc76ac600
MoraPresence <od34156la@mail.ru>
2020-02-09 12:33:13 +0300

    version 0.0.1

--- include/header.hpp
+++ include/header.hpp
@@ -2,5 +2,42 @@
 
 #ifndef INCLUDE_HEADER_HPP_
 #define INCLUDE_HEADER_HPP_
+#include <cstdio>
 
+#define STACK_OVERFLOW  -100
+#define STACK_UNDERFLOW -101
+#define OUT_OF_MEMORY   -102
+
+template <typename T>
+class stackOBJ{
+public:
+    T data;
+    stackOBJ *next_stackOBJ;
+};
+
+
+template <typename T>
+class stack
+{
+public:
+    stack<T> () {}
+    stack(size_t countStack) : _countStack(countStack) {}
+
+    ~stack(){
+        while(top){
+            auto tmp = top;
+            top = top->next_stackOBJ;
+            delete tmp;
+        }
+    }
+
+    void push(T&& value);
+    void push(const T& value);
+    void pop();
+    const T& head() const;
+
+private:
+    stackOBJ<T> *top = NULL;
+    size_t _countStack = 10;
+};
 #endif // INCLUDE_HEADER_HPP_
--- sources/source.cpp
+++ sources/source.cpp
@@ -1,3 +1,52 @@
 // Copyright 2018 Your Name <your_email>
 
 #include <header.hpp>
+#include <iostream>
+
+
+template<typename T>
+const T &stack<T>::head() const {
+    return top->data;
+}
+
+
+template<typename T>
+void stack<T>::push(const T &value) {
+    if (_countStack) {
+
+        stackOBJ<T> *node = new stackOBJ<T>;
+        node->data = value;
+        node->next_stackOBJ = top;
+        top = node;
+        --_countStack;
+    } else {
+        exit(STACK_OVERFLOW);
+    }
+}
+
+template<typename T>
+void stack<T>::pop() {
+    T value;
+    if (top) {
+        stackOBJ<T> *old = top;
+        top = top->next_stackOBJ;
+        value = old->data;
+        delete old;
+    } else {
+        exit(STACK_UNDERFLOW);
+    }
+}
+
+
+template<typename T>
+void stack<T>::push(T &&value) {
+    if (_countStack) {
+        stackOBJ<T> *node = new stackOBJ<T>;
+        node->data = value;
+        node->next_stackOBJ = top;
+        top = node;
+        --_countStack;
+    } else {
+        exit(STACK_OVERFLOW);
+    }
+}
