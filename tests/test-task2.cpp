2ad6b89324f83a1507cbf535b9e1bbfd61da9198
MoraPresence <od34156la@mail.ru>
2020-02-11 10:38:42 +0300

    version 1.2.5

---
+++ tests/test-task2.cpp
@@ -0,0 +1,29 @@
+// Copyright 2018 Your Name <your_email>
+
+#include <gtest/gtest.h>
+#include <header-task2.hpp>
+
+TEST(Stack, test_5){
+        class A{
+    public:
+    A(int a, int b) {
+        c = a + b;
+    }
+        int c;
+    };
+    myStack<A> s{};
+
+    s.push_emplace(4, 6, 2);
+    auto a = s.head;
+    EXPECT_EQ(a.c, 12);
+}
+
+TEST(Stack, test_6){
+    myStack<int> s{};
+
+    s.push(6);
+    s.push(9);
+    s.pop();
+
+    EXPECT_EQ(s.head(), 6);
+}
